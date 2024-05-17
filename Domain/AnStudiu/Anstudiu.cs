f187eea6d8d3196d55a6277e3a6c68c046ef66b8
Mioara98 <98015216+Mioara98@users.noreply.github.com>
2024-05-17 18:59:17 +0300

    nou
    sds

---
+++ Domain/AnStudiu/Anstudiu.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.AnStudiu
+{
+    public class AnStudiu
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }  
+        public List <Domain.Semestru.Semestru> Semestru { get; set; }
+
+        public string NumeAnStudiu { get; set; }
+
+    }
+}
